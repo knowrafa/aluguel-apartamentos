31cb3aef2836191ddfc69f5ccd35b1bcba3076dc
rafael <noprafa@gmail.com>
2021-08-26 17:21:19 -0300

    Criando icon

--- apartamentos_aluguel/settings.py
+++ apartamentos_aluguel/settings.py
@@ -108,17 +108,17 @@ WSGI_APPLICATION = "apartamentos_aluguel.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": os.environ.get("DATABASE_NAME"),
-#         "HOST": os.environ.get("DATABASE_HOST"),
-#         "USER": os.environ.get("DATABASE_USER"),
-#         "PASSWORD": os.environ.get("DATABASE_PASSWORD"),
-#     },
-# }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("DATABASE_NAME"),
+        "HOST": os.environ.get("DATABASE_HOST"),
+        "USER": os.environ.get("DATABASE_USER"),
+        "PASSWORD": os.environ.get("DATABASE_PASSWORD"),
+    },
+}
 
-DATABASES = {"default": dj_database_url.config()}
+# DATABASES = {"default": dj_database_url.config()}
 
 
 PROJECT_APPS = [
--- apartamentos_aluguel/urls.py
+++ apartamentos_aluguel/urls.py
@@ -14,9 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.generic.base import RedirectView
+from django.conf import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('apartamentos.urls')),
+    path("admin/", admin.site.urls),
+    path("", include("apartamentos.urls")),
+    path("favicon.ico", RedirectView.as_view(url=settings.STATIC_URL + "favicon.ico")),
 ]
